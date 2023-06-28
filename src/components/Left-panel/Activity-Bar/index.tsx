f3756536b310803e2fceafda9c18d40fde5768f4
Nygen <nducminh002@gmail.com>
2023-06-28 20:15:53 +0200

    feat: fontawesome icons and library

--- src/components/Left-panel/Activity-Bar/index.tsx
+++ src/components/Left-panel/Activity-Bar/index.tsx
@@ -1,22 +1,29 @@
 import styles from "./style.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faCircleStop,
+} from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
-const tabs = [
+const tabs: { label: IconDefinition }[] = [
   {
-    label: "😃",
+    label: faMagnifyingGlass,
   },
   {
-    label: "😃",
-  },
-  {
-    label: "😃",
+    label: faCircleStop,
   },
 ];
 
 const ActivityBar = () => {
   return (
     <ul className={styles.activity_bar}>
-      {tabs.map((tab, index) => {
-        return <li key={index}>{tab.label}</li>;
+      {tabs.map((tab: { label: IconDefinition }, index) => {
+        return (
+          <li key={index}>
+            <FontAwesomeIcon icon={tab.label} />
+          </li>
+        );
       })}
     </ul>
   );
--- src/components/Left-panel/Primary-Side-Bar/index.tsx
+++ src/components/Left-panel/Primary-Side-Bar/index.tsx
@@ -1,23 +1,45 @@
 import styles from "./style.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  IconDefinition,
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
+interface IWorkspace {
+  name: string;
+  folders?: {
+    icon: IconDefinition;
+    files: string;
+  }[];
+}
+
+const workspace: IWorkspace[] = [
+  { name: "personal life", folders: [{ icon: faChevronDown, files: "alfa" }] },
+  { name: "contact" },
+];
 
 const PrimarySideBar = () => {
   return (
     <ul className={styles.primary_side_bar}>
       <li>
-        <h2>1.workspace</h2>
+        <h2>
+          <FontAwesomeIcon icon={faChevronDown} />
+          1.workspace
+        </h2>
         <ul>
-            <li>alfa</li>
-            <li>beta</li>
-            <li>gamma</li>
-
+          <li>alfa</li>
+          <li>beta</li>
+          <li>gamma</li>
         </ul>
       </li>
       <li>
-        <h2>2.workspace</h2>
+        <h2>
+          <FontAwesomeIcon icon={faChevronDown} />
+          2.workspace
+        </h2>
         <ul>
-            <li>ichi</li>
-            <li>ni</li>
-            <li>san</li>
+          <li>ichi</li>
+          <li>ni</li>
+          <li>san</li>
         </ul>
       </li>
     </ul>
