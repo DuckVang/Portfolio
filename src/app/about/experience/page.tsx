7cf0ada37ec799e087c3420107bb6c30e994724f
Nygen <nducminh002@gmail.com>
2023-07-16 22:06:20 +0200

    feat: experience section (no content and styles)

---
+++ src/app/about/experience/page.tsx
@@ -0,0 +1,23 @@
+import styles from "./page.module.scss";
+import helpers from "../../styles/helpers.module.scss";
+import Link from "next/link";
+import CanvasAnimation from "@/components/Three-Animation";
+import LeftPanel from "@/components/Left-panel";
+
+import Text from "@/components/Text";
+
+export default function Projets() {
+  const example = `Aute qui labore proident officia mollit. Tempor incididunt duis magna
+    incididunt proident minim ipsum cillum. Reprehenderit ad duis nulla
+    minim officia anim ad labore ut elit incididunt dolore. Aliquip
+    officia incidi`;
+
+  return (
+    <main className={styles.main}>
+      
+pog
+
+
+    </main>
+  );
+}
