f0ff00bcd8330e8f61a88c16d80fe2185080160f
Nygen <nducminh002@gmail.com>
2023-07-23 17:13:07 +0200

    fix: type definition

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { createContext } from "react";
+import React, { Dispatch, createContext, useState } from "react";
 
-export const HistoryContext = createContext("");
+export const HistoryContext = createContext<
+  | [string[] | null, React.Dispatch<React.SetStateAction<string[] | null>>]
+  | null
+>(null);
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const [history, setHistory] = useState<string[] | null>(["none"]);
+
   return (
-    <HistoryContext.Provider value={"pog"}>{children}</HistoryContext.Provider>
+    <HistoryContext.Provider value={[history,setHistory]}>
+      {children}
+    </HistoryContext.Provider>
   );
 }
