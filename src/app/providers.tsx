bec8c086a454157cdfb318125bf306b153689aa6
Nygen <nducminh002@gmail.com>
2023-07-23 19:22:20 +0200

    feat: change to reducer

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,46 @@
 "use client";
 
-import React, { Dispatch, createContext, useState } from "react";
+import React, {
+  Dispatch,
+  Reducer,
+  ReducerAction,
+  createContext,
+  useReducer,
+  useState,
+} from "react";
 
-export const HistoryContext = createContext<
-  | [string[] | null, React.Dispatch<React.SetStateAction<string[] | null>>]
-  | null
->(null);
+enum actions {
+  ADD = "ADD_TODO_ITEM",
+  REMOVE = "REMOVE_TODO_ITEM",
+}
+
+type Action = {
+  type: actions;
+  payload: string;
+};
+
+type initialStateType = {
+  history: string[];
+};
+
+const reducer: Reducer<initialStateType, Action> = (state, action) => {
+  switch (action.type) {
+    case actions.ADD:
+      return { history: [...state.history, action.payload] };
+    default:
+      return state;
+  }
+};
+export const HistoryContext = createContext<{
+  state: initialStateType;
+  dispatch: React.Dispatch<Action>;
+}>({ state: { history: [] }, dispatch: () => undefined });
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [history, setHistory] = useState<string[] | null>(["none"]);
+  const [state, dispatch] = useReducer(reducer, { history: [] });
 
   return (
-    <HistoryContext.Provider value={[history,setHistory]}>
+    <HistoryContext.Provider value={{state,dispatch}}>
       {children}
     </HistoryContext.Provider>
   );
