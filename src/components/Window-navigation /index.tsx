fb3d8145b695f7ceaa4fe0083babda879a4a2a83
Nygen <nducminh002@gmail.com>
2023-07-24 19:17:49 +0200

    feat: history context rendering

--- src/components/Window-navigation /index.tsx
+++ src/components/Window-navigation /index.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
-import styles from "./style.module.scss"
+import styles from "./style.module.scss";
+import { useContext } from "react";
+import { HistoryContext } from "@/app/providers";
+
 const WindowNavigation = (props: any) => {
+  const { state, dispatch } = useContext(HistoryContext);
   return (
     <nav className={styles.navigation}>
       <ul>
-        <li>
+        {/* <li>
           <Link href="/about/projects">File</Link>
         </li>
         <li>Edit</li>
-        <li>Selection</li>
+        <li>Selection</li> */}
+
+        {state.history.map((link, index) => {
+          return (
+            <li key={index}>
+              {link.length > 0 ? (
+                <Link href={"/workspace/" + link}>{link}</Link>
+              ):<Link href={"/workspace/" + link}>{"about"}</Link>}
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
